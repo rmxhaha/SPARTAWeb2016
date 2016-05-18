0b5e5c2d9e6a5ffa79f401dffc249f83c432b0a6
rmxhaha <code000123@gmail.com>
2016-05-19 00:12:20 +0700

    layout uses bootstrap

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+app.use('/bower_components',  express.static(path.join(__dirname, '/bower_components')));
 
 app.use('/', routes);
 app.use('/users', users);
