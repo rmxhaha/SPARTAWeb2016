5a29c41103b2b1e328c6d42ac67d97315b0a46e2
rmxhaha <code000123@gmail.com>
2016-05-21 02:04:05 +0700

    add tugas and list tugas added

--- routes/tugas.js
+++ routes/tugas.js
@@ -1,17 +1,36 @@
 var express = require("express");
 var router = express.Router();
+var dbconf = require('../conf/db');
+var mysql = require('mysql');
+
 
 
 router.get('/', function(req, res, next) {
-   res.render("tugas", { list_tugas : [[1,"tugas #1"]]});
+   var conn = mysql.createConnection(dbconf);
+   conn.connect();
+   conn.query("SELECT id,nama_tugas FROM tugas;", function(err,rows,fields){
+      if(err) throw err;
+      console.log( rows );
+      res.render("tugas", { list_tugas : rows });
+   });
+   conn.end();   
 });
 
 router.post('/submit', function(req, res, next){
   // add tugas ke db di sini
-  
+  var conn = mysql.createConnection(dbconf);
+  conn.connect();
+
+  var query = "INSERT INTO tugas (`nama_tugas`) VALUES ('" + req.body.nama + "');";
+
+  conn.query( query );
+
+  conn.end();  
   res.redirect('./');
 });
+
 router.get('/submit', function(req,res){ res.redirect('./')});
+
 router.get('/delete', function(req,res){
   res.redirect('./');
 });
