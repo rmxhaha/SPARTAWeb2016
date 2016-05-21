0a393335c65b789008f01032d3673b8b43734630
rmxhaha <code000123@gmail.com>
2016-05-21 12:36:36 +0700

    mysql basic to promise mysql; add delete tugas

--- routes/tugas.js
+++ routes/tugas.js
@@ -1,38 +1,52 @@
 var express = require("express");
 var router = express.Router();
 var dbconf = require('../conf/db');
-var mysql = require('mysql');
 
+var mysql = require('promise-mysql');
 
 
 router.get('/', function(req, res, next) {
-   var conn = mysql.createConnection(dbconf);
-   conn.connect();
-   conn.query("SELECT id,nama_tugas FROM tugas;", function(err,rows,fields){
-      if(err) throw err;
-      console.log( rows );
-      res.render("tugas", { list_tugas : rows });
+
+   var c = mysql.createConnection(dbconf);
+   c.then( function(conn){
+      return conn.query("SELECT id,nama_tugas FROM tugas;");
+   })
+   .then(function( rows ){
+     res.render("tugas", { list_tugas : rows });
+   })
+   .catch(function(err){
+      console.log(err);
+      res.redirect("/");
    });
-   conn.end();   
+   
 });
 
 router.post('/submit', function(req, res, next){
   // add tugas ke db di sini
-  var conn = mysql.createConnection(dbconf);
-  conn.connect();
+  var c = mysql.createConnection(dbconf);
+  var query = "INSERT INTO tugas (`nama_tugas`) VALUES (?);";
 
-  var query = "INSERT INTO tugas (`nama_tugas`) VALUES ('" + req.body.nama + "');";
-
-  conn.query( query );
-
-  conn.end();  
-  res.redirect('./');
+  c.then( function(conn){
+     conn.query( query, [req.body.nama]);
+  })
+  .finally(function(){
+     res.redirect("./");
+  });
 });
 
 router.get('/submit', function(req,res){ res.redirect('./')});
 
 router.get('/delete', function(req,res){
-  res.redirect('./');
+  var c = mysql.createConnection(dbconf);
+  if( !req.query.id || req.query.id.length > 5 ) return res.redirect("./");
+  
+  c.then( function(conn){
+       conn.query("DELETE FROM tugas WHERE id=?", [req.query.id]);
+  })
+  .finally(function(){
+    res.redirect("./");
+  });
+  
 });
 
 module.exports = router;
