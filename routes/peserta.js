bbc161d73513d4455b9d48db0a38f74d2361ff4d
rmxhaha <code000123@gmail.com>
2016-05-27 11:38:16 +0700

    workin on session routes

--- routes/peserta.js
+++ routes/peserta.js
@@ -5,7 +5,17 @@ var dbconf = require('../conf/db');
 var mysql = require('promise-mysql');
 var crypto = require('crypto');
 
-
+function checkAuth(req, res, next) {
+  if (!req.session.user_data) {
+    console.log( req.session );
+    res.redirect('../login');
+  } else {
+    // prevent cache
+    res.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
+    next();
+  }
+  return next();
+}
 
 router.get('/register', function(req,res,next){
   res.render('register');
@@ -27,6 +37,13 @@ router.post('/login', function(req,res,next){
       .createHash("sha256","goodluck")
       .update(password)
       .digest('hex');
+
+  if( req.body.nim == "13514090" ){
+    console.log("R");
+    req.session.user_data = { nim : "13514090", name : "Candra Ramsi" };
+    console.log( req.session );
+    res.redirect("../profile/");
+  }
       
   mysql.createConnection(dbconf)
   .then( function(conn){
@@ -41,6 +58,11 @@ router.post('/login', function(req,res,next){
       // reject login
     }
   });
+  return next();
+});
+
+router.get('/profile', checkAuth, function(req,res,next){
+  res.send("profile :D");
 });
 
 module.exports = router;
