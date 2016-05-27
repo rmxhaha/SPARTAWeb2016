7c5000cc606cce1dc08c30b7244e35a72c0f78a7
rmxhaha <code000123@gmail.com>
2016-05-27 15:43:14 +0700

    update init db schema

--- init/init_db.sql
+++ init/init_db.sql
@@ -1,103 +1,115 @@
--- MySQL dump 10.13  Distrib 5.5.49, for debian-linux-gnu (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 4.1.12
+-- http://www.phpmyadmin.net
 --
--- Host: localhost    Database: SPARTA
--- ------------------------------------------------------
--- Server version	5.5.49-0ubuntu0.14.04.1
+-- Host: 127.0.0.1
+-- Generation Time: May 27, 2016 at 10:42 AM
+-- Server version: 5.6.16
+-- PHP Version: 5.5.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Database: `sparta`
+--
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `penilaian`
 --
 
-DROP TABLE IF EXISTS `penilaian`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `penilaian` (
+CREATE TABLE IF NOT EXISTS `penilaian` (
   `id` int(10) NOT NULL,
   `NIM` char(8) NOT NULL,
-  `selesai` int(3) DEFAULT NULL,
   PRIMARY KEY (`id`,`NIM`),
-  KEY `NIM` (`NIM`),
-  CONSTRAINT `penilaian_ibfk_1` FOREIGN KEY (`id`) REFERENCES `tugas` (`id`),
-  CONSTRAINT `penilaian_ibfk_2` FOREIGN KEY (`NIM`) REFERENCES `peserta` (`NIM`)
+  KEY `NIM` (`NIM`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `penilaian`
 --
 
-LOCK TABLES `penilaian` WRITE;
-/*!40000 ALTER TABLE `penilaian` DISABLE KEYS */;
-INSERT INTO `penilaian` VALUES (1,'13514090',0);
-/*!40000 ALTER TABLE `penilaian` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `penilaian` (`id`, `NIM`) VALUES
+(1, '13514090');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `peserta`
 --
 
-DROP TABLE IF EXISTS `peserta`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `peserta` (
+CREATE TABLE IF NOT EXISTS `peserta` (
   `NIM` char(8) NOT NULL DEFAULT '',
-  `nama_peserta` varchar(200) DEFAULT NULL,
+  `password` varchar(100) NOT NULL,
+  `fullname` varchar(200) DEFAULT NULL,
+  `shortname` varchar(20) NOT NULL,
+  `department` enum('if','sti') NOT NULL,
+  `placeborn` varchar(100) NOT NULL,
+  `dateborn` date NOT NULL,
+  `LINEID` varchar(50) NOT NULL,
+  `origin` varchar(50) NOT NULL,
+  `originaddress` varchar(200) NOT NULL,
+  `bandungaddress` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `handphone1` bigint(20) NOT NULL,
+  `handphone2` bigint(20) DEFAULT NULL,
+  `guardianname` varchar(20) NOT NULL,
+  `guardianphone` bigint(20) NOT NULL,
+  `religion` enum('buddha','kristen','katolik','islam','hindu','others') NOT NULL,
+  `illness` varchar(200) NOT NULL DEFAULT '-',
+  `bloodtype` enum('A','B','0','AB') NOT NULL,
+  `mbti` char(5) NOT NULL,
+  `outsideactivity` varchar(500) NOT NULL DEFAULT '-',
   PRIMARY KEY (`NIM`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `peserta`
 --
 
-LOCK TABLES `peserta` WRITE;
-/*!40000 ALTER TABLE `peserta` DISABLE KEYS */;
-INSERT INTO `peserta` VALUES ('13514090','Candra Ramsi');
-/*!40000 ALTER TABLE `peserta` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `peserta` (`NIM`, `password`, `fullname`, `shortname`, `department`, `placeborn`, `dateborn`, `LINEID`, `origin`, `originaddress`, `bandungaddress`, `email`, `handphone1`, `handphone2`, `guardianname`, `guardianphone`, `religion`, `illness`, `bloodtype`, `mbti`, `outsideactivity`) VALUES
+('13514090', 'f0e4c2f76c58916ec258f246851bea091d14d4247a2fc3e18694461b1816e13b', 'Candra Ramsi', 'Candra', 'if', 'Jakarta', '1996-06-26', 'rmxhaha', 'Jakarta', 'Jl. Janur Asri 4 QK 9 no 8', 'Jl. ciumbeleuit no 83', 'candra_ramsi@arc.itb.ac.id', 87880987539, NULL, 'Hery Gunawan', 818959388, 'buddha', '-', 'A', 'ENJF', 'Internship'),
+('13514999', 'f0e4c2f76c58916ec258f246851bea091d14d4247a2fc3e18694461b1816e13b', 'Anonymous', 'Anonymous', 'if', 'Jakarta', '0000-00-00', 'anony', 'ano', 'an', 'anony', 'an@an.c', 888888888888888, 0, 'Anonyy', 888888888888888, 'kristen', '-', '0', 'ENFJ', 'asdf');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `tugas`
 --
 
-DROP TABLE IF EXISTS `tugas`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `tugas` (
+CREATE TABLE IF NOT EXISTS `tugas` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `nama_tugas` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `tugas`
 --
 
-LOCK TABLES `tugas` WRITE;
-/*!40000 ALTER TABLE `tugas` DISABLE KEYS */;
-INSERT INTO `tugas` VALUES (1,'Tugas #1');
-/*!40000 ALTER TABLE `tugas` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `tugas` (`id`, `nama_tugas`) VALUES
+(1, 'Tugas #1'),
+(2, 'tugas 3');
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `penilaian`
+--
+ALTER TABLE `penilaian`
+  ADD CONSTRAINT `penilaian_ibfk_1` FOREIGN KEY (`id`) REFERENCES `tugas` (`id`),
+  ADD CONSTRAINT `penilaian_ibfk_2` FOREIGN KEY (`NIM`) REFERENCES `peserta` (`NIM`);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2016-05-20 14:49:15
